4d0589b699d67863fba72f28848e9bfc836c229a
Karan Janthe <karanjanthe@gmail.com>
2021-12-19 14:03:15 +0530

    first commit

---
+++ src/Footer.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Footer() {
+    return (
+        <div>
+            Made by <a href="https://linkedin.com/in/KaranJanthe" target="_blank"  rel="noreferrer">Karan Janthe</a>
+        </div>
+    )
+}
